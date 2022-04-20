3427ec1ee9046c9610281e8fb89ec7d43626de2a
Helmut Januschka <helmut@januschka.com>
2022-04-20 14:02:52 +0200

    Update vcl_load.go

--- vcl_load.go
+++ vcl_load.go
@@ -9,7 +9,7 @@ import (
 // LoadVCL compiles and loads the VCL file from the given file.
 // See https://varnish-cache.org/docs/trunk/reference/varnish-cli.html#vcl-load-configname-filename-auto-cold-warm
 func (c *Client) LoadVCL(ctx context.Context, configname, filename string, mode VCLState) error {
-	args := []string{strconv.Quote(configname), strconv.Quote(filename), string(mode)}
+	args := []string{configname, strconv.Quote(filename), string(mode)}
 	resp, err := c.roundtrip.Execute(ctx, &Request{"vcl.load", args})
 	if err != nil {
 		return err
