94a389e5801fdab33121acaf8d18cedc278a580e
Martin Helmich <m.helmich@mittwald.de>
2023-05-25 21:16:32 +0200

    Move integrations tests to official Varnish image

--- param_list_test.go
+++ param_list_test.go
@@ -14,7 +14,9 @@ func TestParameterList(t *testing.T) {
 		t.Error(err)
 	}
 
-	assert.Len(t, params, 104)
+	// Don't assert on the exact number of parameters, as this may change
+	// between varnish versions
+	assert.GreaterOrEqual(t, len(params), 100)
 	assert.Equal(t, "acceptor_sleep_incr", params[2].Name)
 	assert.Equal(t, "0.000", params[2].Value)
 	assert.Equal(t, "seconds", params[2].Unit)
